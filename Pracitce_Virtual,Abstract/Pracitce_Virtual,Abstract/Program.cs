227464d5f7db49e1cc6160e0d70cc1a94cf2cfb5
2018007838 <2018007838@hanyang.ac.kr>
2020-12-28 16:14:18 +0900

    최종본

--- Pracitce_Virtual,Abstract/Pracitce_Virtual,Abstract/Program.cs
+++ Pracitce_Virtual,Abstract/Pracitce_Virtual,Abstract/Program.cs
@@ -13,7 +13,10 @@ namespace Pracitce_Virtual_Abstract
             Console.WriteLine("GetData function - " + GetData());
         }
 
-        protected abstract string GetData();
+        protected virtual string GetData()
+        {
+            return "AbstractExample";
+        }
     }
     class Example1 : AbstractExample
     {
@@ -24,10 +27,13 @@ namespace Pracitce_Virtual_Abstract
     }
     class Example2 : AbstractExample
     {
+        // virtual은 꼭 재정의 할 필요 없음.
+        /*
         protected override string GetData()
         {
             return "Example2";
         }
+        */
     }
 
     class Program
