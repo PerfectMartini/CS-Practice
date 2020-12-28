5db11495b954ca1b7fe96d356e3237999e774641
2018007838 <2018007838@hanyang.ac.kr>
2020-12-28 16:37:47 +0900

    v1

---
+++ Practice_interface/Practice_interface/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_interface
+{
+    interface IATypeInterface
+    {
+        string GetATypeName();
+        void Print();
+    }
+    interface IBTypeInterface
+    {
+        string GetBTypeName();
+        void Print();
+    }
+    class Example : IATypeInterface, IBTypeInterface
+    {
+        public string GetATypeName()
+        {
+            return "A example";
+        }
+        public string GetBTypeName()
+        {
+            return "B example";
+        }
+        public void Print()
+        {
+            Console.WriteLine("Exmple - GetATypeName - " + GetATypeName());
+            Console.WriteLine("Exmple - GetBTypeName - " + GetBTypeName());
+        }
+    }
+    class AExample : IATypeInterface
+    {
+        public string GetATypeName()
+        {
+            return "A - Example";
+        }
+        public void Print()
+        {
+            Console.WriteLine("Exmple - GetATypeName - " + GetATypeName());
+
+        }
+    }
+    class BExample : IBTypeInterface
+    {
+        public string GetBTypeName()
+        {
+            return "B - Example";
+        }
+        public void Print()
+        {
+            Console.WriteLine("Exmple - GetBTypeName - " + GetBTypeName());
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Example ex = new Example();
+            ex.Print();
+
+            AExample Aex = new AExample();
+            Aex.Print();
+
+            BExample Bex = new BExample();
+            Bex.Print();
+        }
+    }
+}
