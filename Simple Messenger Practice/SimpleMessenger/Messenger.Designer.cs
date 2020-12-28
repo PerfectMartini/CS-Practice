5f5f9f5076c4cb42a5b24c40a31f6cf6a3402173
Martini <48506160+PerfectMartini@users.noreply.github.com>
2020-12-28 14:52:20 +0900

    Revert "Simple messenger 1"

--- Simple Messenger Practice/SimpleMessenger/Messenger.Designer.cs
+++ Simple Messenger Practice/SimpleMessenger/Messenger.Designer.cs
@@ -35,7 +35,7 @@
             this.Radio_B = new System.Windows.Forms.RadioButton();
             this.Button_Connection = new System.Windows.Forms.Button();
             this.Button_Send = new System.Windows.Forms.Button();
-            this.Label_Success = new System.Windows.Forms.Label();
+            this.Label_Succes = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // TextBox_ChatLog
@@ -44,24 +44,23 @@
             this.TextBox_ChatLog.Location = new System.Drawing.Point(12, 92);
             this.TextBox_ChatLog.Multiline = true;
             this.TextBox_ChatLog.Name = "TextBox_ChatLog";
-            this.TextBox_ChatLog.ReadOnly = true;
-            this.TextBox_ChatLog.Size = new System.Drawing.Size(869, 324);
+            this.TextBox_ChatLog.Size = new System.Drawing.Size(664, 247);
             this.TextBox_ChatLog.TabIndex = 0;
             // 
             // TextBox_SendMessage
             // 
             this.TextBox_SendMessage.Font = new System.Drawing.Font("D2Coding", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.TextBox_SendMessage.Location = new System.Drawing.Point(12, 422);
+            this.TextBox_SendMessage.Location = new System.Drawing.Point(12, 345);
             this.TextBox_SendMessage.Multiline = true;
             this.TextBox_SendMessage.Name = "TextBox_SendMessage";
-            this.TextBox_SendMessage.Size = new System.Drawing.Size(735, 59);
+            this.TextBox_SendMessage.Size = new System.Drawing.Size(530, 59);
             this.TextBox_SendMessage.TabIndex = 1;
             // 
             // Label_ChooseClient
             // 
             this.Label_ChooseClient.AutoSize = true;
             this.Label_ChooseClient.Font = new System.Drawing.Font("D2Coding", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.Label_ChooseClient.Location = new System.Drawing.Point(423, 38);
+            this.Label_ChooseClient.Location = new System.Drawing.Point(218, 39);
             this.Label_ChooseClient.Name = "Label_ChooseClient";
             this.Label_ChooseClient.Size = new System.Drawing.Size(280, 22);
             this.Label_ChooseClient.TabIndex = 2;
@@ -71,7 +70,7 @@
             // 
             this.Radio_A.AutoSize = true;
             this.Radio_A.Font = new System.Drawing.Font("D2Coding", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.Radio_A.Location = new System.Drawing.Point(709, 21);
+            this.Radio_A.Location = new System.Drawing.Point(504, 22);
             this.Radio_A.Name = "Radio_A";
             this.Radio_A.Size = new System.Drawing.Size(38, 23);
             this.Radio_A.TabIndex = 3;
@@ -83,7 +82,7 @@
             // 
             this.Radio_B.AutoSize = true;
             this.Radio_B.Font = new System.Drawing.Font("D2Coding", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.Radio_B.Location = new System.Drawing.Point(709, 50);
+            this.Radio_B.Location = new System.Drawing.Point(504, 51);
             this.Radio_B.Name = "Radio_B";
             this.Radio_B.Size = new System.Drawing.Size(38, 23);
             this.Radio_B.TabIndex = 4;
@@ -94,42 +93,39 @@
             // Button_Connection
             // 
             this.Button_Connection.Font = new System.Drawing.Font("D2Coding", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.Button_Connection.Location = new System.Drawing.Point(753, 21);
+            this.Button_Connection.Location = new System.Drawing.Point(548, 22);
             this.Button_Connection.Name = "Button_Connection";
             this.Button_Connection.Size = new System.Drawing.Size(128, 52);
             this.Button_Connection.TabIndex = 5;
             this.Button_Connection.Text = "접속";
             this.Button_Connection.UseVisualStyleBackColor = true;
-            this.Button_Connection.Click += new System.EventHandler(this.Button_Connection_Click);
             // 
             // Button_Send
             // 
             this.Button_Send.Font = new System.Drawing.Font("D2Coding", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.Button_Send.Location = new System.Drawing.Point(771, 422);
+            this.Button_Send.Location = new System.Drawing.Point(566, 344);
             this.Button_Send.Name = "Button_Send";
             this.Button_Send.Size = new System.Drawing.Size(110, 60);
             this.Button_Send.TabIndex = 6;
             this.Button_Send.Text = "전송";
             this.Button_Send.UseVisualStyleBackColor = true;
-            this.Button_Send.Click += new System.EventHandler(this.Button_Send_Click);
             // 
-            // Label_Success
+            // Label_Succes
             // 
-            this.Label_Success.AutoSize = true;
-            this.Label_Success.Font = new System.Drawing.Font("D2Coding", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.Label_Success.Location = new System.Drawing.Point(327, 36);
-            this.Label_Success.Name = "Label_Success";
-            this.Label_Success.Size = new System.Drawing.Size(100, 23);
-            this.Label_Success.TabIndex = 7;
-            this.Label_Success.Text = "접속 성공";
-            this.Label_Success.Visible = false;
+            this.Label_Succes.AutoSize = true;
+            this.Label_Succes.Font = new System.Drawing.Font("D2Coding", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Label_Succes.Location = new System.Drawing.Point(92, 39);
+            this.Label_Succes.Name = "Label_Succes";
+            this.Label_Succes.Size = new System.Drawing.Size(81, 19);
+            this.Label_Succes.TabIndex = 7;
+            this.Label_Succes.Text = "접속 성공";
             // 
             // Messenger
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(893, 494);
-            this.Controls.Add(this.Label_Success);
+            this.ClientSize = new System.Drawing.Size(691, 412);
+            this.Controls.Add(this.Label_Succes);
             this.Controls.Add(this.Button_Send);
             this.Controls.Add(this.Button_Connection);
             this.Controls.Add(this.Radio_B);
@@ -155,7 +151,7 @@
         private System.Windows.Forms.RadioButton Radio_B;
         private System.Windows.Forms.Button Button_Connection;
         private System.Windows.Forms.Button Button_Send;
-        private System.Windows.Forms.Label Label_Success;
+        private System.Windows.Forms.Label Label_Succes;
     }
 }
 
